f3bb09139e17b2bbb263f1c22d52ae244ce4f907
Daria <dshxlva@yandex.ru>
2026-07-06 21:21:14 +0300

    fix endpage buttons and navigation

--- client/src/EndPage.jsx
+++ client/src/EndPage.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 
 export default function EndPage() {
+  const navigate = useNavigate()
   //TODO: add go back button to the title page & button with the party restarted
 
   return (
@@ -9,6 +10,7 @@ export default function EndPage() {
         <h2>Party is over. Want to have fun again?</h2>
         <button onClick={() => navigate('/main')}>Restart</button>
         <button onClick={() => navigate('/')}>Go to the title</button>
+        <button onClick={() => window.location.href = 'https://github.com/dsshji/the-party'}>Star GitHub Repo</button>
       </div>
 
       <footer className="page-footer">
